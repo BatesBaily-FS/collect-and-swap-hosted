5a52667b2b9cea5c14f1bded0a4066f497ea4322
Baily Bates <BnBates1@student.fullsail.edu>
2025-08-09 22:27:40 -0500

    updated

--- client/server.js
+++ client/server.js
@@ -4,8 +4,12 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("*", (req, res) => {
+app.get("/*splat", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.js"));
 });
 
 const PORT = process.env.PORT || 3000;
