49a6b9d0e0be596212267a2ea34b389f31246177
Baily Bates <BnBates1@student.fullsail.edu>
2025-08-09 23:44:04 -0500

    updated

--- client/server.js
+++ client/server.js
@@ -1,18 +1,25 @@
 const express = require("express");
 const path = require("path");
+const { setTimeout } = require("timers/promises");
 const app = express();
-
-app._router.stack.forEach((layer) => {
-  if (layer.route) {
-    console.log(`Defined route: ${layer.route.path}`);
-  }
-});
 
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
+
+setTimeout(() => {
+  if (app._router) {
+    app._router.stack.forEach((layer) => {
+      if (layer.route) {
+        console.log(`Defined route: ${layer.route.path}`);
+      }
+    });
+  } else {
+    console.log("No routes are defined yet.");
+  }
+}, 0);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
