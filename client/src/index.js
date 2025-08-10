ae7c41f5b8e9ce253fc67d1ee586ca9b9db8b7b6
Baily Bates <BnBates1@student.fullsail.edu>
2025-08-09 21:27:19 -0500

    updated

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Auth0Provider } from "@auth0/auth0-react";
 import { BrowserRouter } from "react-router-dom";
+
 // import { APIProvider } from "@vis.gl/react-google-maps";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
